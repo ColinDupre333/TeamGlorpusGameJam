db2ed43c5ba9181b794044313be76081755e57f1
ManuelMurillo18 <manuel.d.murillo.2003@gmail.com>
2025-10-10 15:44:59 -0400

    LaserGameFinished

--- Assets/AssetsManu/LaserGame/Scipts/AlienComponent.cs
+++ Assets/AssetsManu/LaserGame/Scipts/AlienComponent.cs
@@ -5,21 +5,22 @@ public class AlienComponent : MonoBehaviour
     [SerializeField] Transform[] Locations;
     [SerializeField] float speed = 2f;
     private int currentLocationIndex = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
         float remainingDistance = Vector2.Distance(this.gameObject.transform.position, Locations[currentLocationIndex].position);
-        while(remainingDistance >= 0.1f)
+        if(remainingDistance >= 0.1f)
         {
             this.gameObject.transform.position = Vector2.MoveTowards(this.gameObject.transform.position, Locations[currentLocationIndex].position, speed * Time.deltaTime);
         }
-        currentLocationIndex = (currentLocationIndex + 1) % Locations.Length;
+        else
+            currentLocationIndex = (currentLocationIndex + 1) % Locations.Length;
     }
 }
