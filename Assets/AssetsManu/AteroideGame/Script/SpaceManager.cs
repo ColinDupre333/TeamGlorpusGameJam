c732cbcc6c6e906737dec383830ca9afe4ff7dcf
ColinDupre333 <202167407@edu.clg.qc.ca>
2025-10-10 16:59:26 -0400

    JFKVNKJV

---
+++ Assets/AssetsManu/AteroideGame/Script/SpaceManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class SpaceManager : MonoBehaviour
+{
+    public int score = 0;
+    public int scoreToWin = 10;
+    [SerializeField] AsteroidComponent asteroidPrefab;
+    [SerializeField] TMPro.TextMeshProUGUI WinText;
+    [SerializeField] TMPro.TextMeshProUGUI ScoreText;
+    public int asteroidCount = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        ScoreText.text = score.ToString() + "/" + scoreToWin.ToString();
+        WinText.enabled = false;
+        if (asteroidCount == 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                SpawnAsteroid();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreText.text = score.ToString() + "/" + scoreToWin.ToString();
+        if (asteroidCount == 0)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                SpawnAsteroid();
+            }
+        }
+        if(score >= scoreToWin)
+        {
+            WinText.enabled = true;
+        }
+    }
+    private void SpawnAsteroid()
+    {
+        float offset = Random.Range(0, 1f);
+        Vector2 viewportSpawnPosition = Vector2.zero;
+        int edge = Random.Range(0, 4);
+        if (edge == 0)
+        {
+            viewportSpawnPosition = new Vector2(offset, 0f);
+
+        }
+        else if (edge == 1)
+        {
+            viewportSpawnPosition = new Vector2(offset, 1f);
+        }
+        else if (edge == 2)
+        {
+            viewportSpawnPosition = new Vector2(0f, offset);
+        }
+        else if (edge == 3)
+        {
+            viewportSpawnPosition = new Vector2(1f, offset);
+        }
+        Vector2 worldSpawnPosition = Camera.main.ViewportToWorldPoint(viewportSpawnPosition);
+        AsteroidComponent asteroid = Instantiate(asteroidPrefab, worldSpawnPosition, Quaternion.identity);
+        asteroid.spaceManager = this;
+    }
+}
---
+++ Assets/AssetsManu/LaserGame/Scipts/Laser.cs
@@ -0,0 +1,41 @@
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] Transform Gun;
+    [SerializeField] TMPro.TextMeshProUGUI Finito;
+    public LayerMask layersToHit;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Finito.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = transform.eulerAngles.z * Mathf.Deg2Rad;
+        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 50f, layersToHit);
+        if (hit.collider == null)
+        {
+            transform.localScale = new Vector3(50f, transform.localScale.y, 1);
+            return;
+        }
+        
+        if(hit.collider.tag == "UVO")
+        {
+            transform.localScale = new Vector3((hit.collider.transform.position.x - Gun.transform.position.x) / 2.3f, transform.localScale.y, 1);
+
+        }
+        if (hit.collider.tag == "Ennemi")
+        {
+            transform.localScale = new Vector3((hit.collider.transform.position.x - Gun.transform.position.x ) /2.3f , transform.localScale.y, 1);
+            Destroy(hit.collider.gameObject);
+            Finito.enabled = true;
+
+
+        }
+    }
+}
