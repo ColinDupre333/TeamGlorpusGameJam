8e57a10776c764640b7344831ec4230bcf1160f7
ManuelMurillo18 <manuel.d.murillo.2003@gmail.com>
2025-10-10 12:52:48 -0400

    SpaceAstroid

---
+++ Assets/AssetsManu/AteroideGame/Script/AsteroidComponent.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class AsteroidComponent : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //[SerializeField] GameObject manager;
+    //[SerializeField] GameObject SpaceShip;
+     public SpaceShip spaceShip;
+    public SpaceManager spaceManager;
+    void Start()
+    {
+        // spaceShip  = SpaceShip.GetComponent<SpaceShip>();
+        //spaceManager = manager.GetComponent<SpaceManager>();
+        Rigidbody2D asteroidRigidBody = GetComponent<Rigidbody2D>();
+        Vector2 direction = new Vector2(Random.value, Random.value).normalized;
+        float speed = Random.Range(2f, 5f);
+        asteroidRigidBody.AddForce(direction * speed, ForceMode2D.Impulse);
+        spaceManager.asteroidCount++;
+        Debug.Log(spaceManager.asteroidCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            spaceShip.score += 1;
+            Debug.Log("Score: " + spaceShip.score);
+            Destroy(collision.gameObject);
+            spaceManager.asteroidCount--;
+            Destroy(gameObject);
+            Debug.Log(spaceManager.asteroidCount);
+        }
+        
+    }
+
+   
+
+}
